6ff7f9ec619ab7964a9e0838413fa3a77485e6eb
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2024-10-26 21:03:48 -0300

    init

---
+++ backend/src/main/java/br/ufrn/imd/pastora/PastoraApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.pastora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastoraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/ufrn/imd/pastora/PastoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.pastora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
